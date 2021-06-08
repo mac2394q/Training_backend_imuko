4e05a18f6b5a905ae503141736813be58d63708d
Miguel Angel Claros Quintero <mac2394q@gmail.com>
2021-06-08 17:16:11 -0500

    backend go and dependency sum  and mod

---
+++ backend_go/person.go
@@ -0,0 +1,21 @@
+package main
+
+type Person struct {
+	// _id string `json:"id,omitempty"`
+	// FirstName	string `json:"firstname,omitempty"`
+	// LastName	string `json:"lastname,omitempty"`
+	// Email	string `json:"email,omitempty"`
+	// Age	int `json:"age,omitempty"`
+	schema_version int ,
+    document_version int,
+    fecha_creacion string ,
+    fecha_ultima_modificacion string,
+    codigo_interno: string,
+    primer_nombre string ,
+    segundo_nombre string,
+    primer_apellido string,
+    segundo_apellido string,
+    estado_civil string ,
+    sexo string ,
+
+}
