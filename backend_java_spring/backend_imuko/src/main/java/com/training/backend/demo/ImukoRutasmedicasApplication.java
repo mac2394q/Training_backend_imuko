cf8418b01bc754bb81e7f1f79dba3a2c8893ea31
Miguel Angel Claros Quintero <mac2394q@gmail.com>
2021-06-08 16:26:16 -0500

    add backend spring , reorden directory

---
+++ backend_java_spring/backend_imuko/src/main/java/com/training/backend/demo/ImukoRutasmedicasApplication.java
@@ -0,0 +1,13 @@
+package com.training.backend.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ImukoRutasmedicasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ImukoRutasmedicasApplication.class, args);
+	}
+
+}
